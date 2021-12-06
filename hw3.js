950a9636750ed096a3930e66347ca14b48fcf02a
G2rahman <94071678+G2rahman@users.noreply.github.com>
2021-12-06 13:53:50 -0500

    Update hw3.js

--- hw3.js
+++ hw3.js
@@ -1,11 +1,11 @@
 var prompt= require("prompt-sync")();
 let phrase=String(prompt("Text))
 let stringArray=string.split("")
-let word=String(rompt("How many times has a word been repeated?:"))
+let word=String(prompt("How many times has a word been repeated?:"))
 number=0
 for(let i=0;i<stringArray.length;i++){
     if(stringArray[i].toLowercase()===word.toLowerCase()){
         number++
     }
 }
-console.log((`"${word}" is in the paragraph/sentence ${counter} times`)
+console.log(("${word}" is in the phrase ${counter} times")
