cd6b53a6ec3dc1d8dbc2ff9c7c71ed836fdccb37
G2rahman <94071678+G2rahman@users.noreply.github.com>
2021-12-06 13:50:54 -0500

    Add files via upload

--- javastring.js
+++ javastring.js
@@ -1,2 +1,15 @@
-let str = "Apple, Banana, Kiwi";
-let part = str.slice(7, 13);
+prompt = require("prompt-sync")();
+
+let str = String(prompt("Check for palindrome:"));
+let reversed = "";
+
+for (let i = str.length - 1; i >= 0; i--) {
+  reversed += str[i]
+}
+
+if (str === reversed) {
+  console.log("It is a palindrome")
+} else {
+  console.log("It is not a palindrome")
+}
+
